44eeed1e78d592daf27228e94ff9fe0fc40077bb
Abraham Fergie <jhnfgie1989@gmail.com>
2017-03-02 11:46:02 -0800

    fibonacci complete

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,7 +1,9 @@
-let arr = []
 export default function fibonacci(num){
-  for (var i = 0; i <= num; i = i + i) {
-    arr.push(i)
+  let fib = []
+  fib[0] = 0
+  fib[1] = 1
+  for(let i = 2; i < num; i++){
+    fib[i] = fib[i - 2] + fib[i - 1]
   }
-  return arr
+  return fib
 }
