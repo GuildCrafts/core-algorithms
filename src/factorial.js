82dc880b0b8d8d779b1654dcafd25f590814dded
SomayaB <somaya.bounouar@gmail.com>
2017-04-25 11:00:58 -0700

    Complete factorial algorithm w tests

---
+++ src/factorial.js
@@ -0,0 +1,17 @@
+// factorial
+//
+// Return the factorial of a number.
+//
+// factorial(5)
+// // => 120
+
+export default function factorial(num) {
+var factorialElements = [num]
+for (var i = num; i > 1; i--) {
+factorialElements.push(i - 1)
+}
+console.log('array', factorialElements)
+return factorialElements.reduce(function(a,b){
+return a*b
+}, 1)
+}
