cc318030c44fe639f7ba5445321eb60229ee1c42
Jonathan Pool <pool@panlex.org>
2017-05-03 14:23:26 -0700

    Brought 3+ condition into the if-then-else construction. Added missing return statement.

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -2,7 +2,7 @@
   Define a function to perform a uniform binary search for a specified number in an array
   of number elements sorted in non-descending order and to return the index of some
   instance of the number in the array, or undefined if the array contains no instance of
-  the number. The function’s ruturn value is not guaranteed when any precondition is
+  the number. The function’s return value is not guaranteed when any precondition is
   violated.
 
   Preconditions:
@@ -49,44 +49,56 @@ var binarySearch = function(toFind, ascendingArray) {
     // Otherwise, i.e. if neither of its elements is equal to the number:
     else {
       // Return a failure result.
+      return undefined;
     }
   }
+  // Otherwise, i.e. if it has length 3 or more:
+  else {
   // Until the active subarray is no longer splittable, i.e. until its length is 2 or less:
-  while (startIndex + 1 < endIndex) {
-    // If its first element is equal to the number:
-    if (ascendingArray[startIndex] === toFind) {
-      // Return its index.
-      return startIndex;
-    }
-    // Otherwise, if it is smaller than the number:
-    else if (ascendingArray[startIndex] < toFind) {
-      // If its last element is equal to the number:
-      if (ascendingArray[endIndex] === toFind) {
+    while (startIndex + 1 < endIndex) {
+      // If its first element is equal to the number:
+      if (ascendingArray[startIndex] === toFind) {
         // Return its index.
-        return endIndex;
+        return startIndex;
       }
-      // Otherwise, if it is greater than the number:
-      else if (ascendingArray[endIndex] > toFind) {
-        // Identify a splitting element.
-        splitIndex = Math.floor((startIndex + endIndex) / 2);
-        // If it is smaller than the number:
-        if (ascendingArray[splitIndex] < toFind) {
-          // Make it the start of the active subarray.
-          startIndex = splitIndex;
-        }
-        // Otherwise, if it is greater than the number
-        else if (ascendingArray[splitIndex] > toFind) {
-          // Make it the end of the active subarray.
-          endIndex = splitIndex;
-        }
-        // Otherwise, i.e. if it is equal to the number:
-        else {
+      // Otherwise, if it is smaller than the number:
+      else if (ascendingArray[startIndex] < toFind) {
+        // If its last element is equal to the number:
+        if (ascendingArray[endIndex] === toFind) {
           // Return its index.
-          return splitIndex;
+          return endIndex;
+        }
+        // Otherwise, if it is greater than the number:
+        else if (ascendingArray[endIndex] > toFind) {
+          // Identify a splitting element.
+          splitIndex = Math.floor((startIndex + endIndex) / 2);
+          // If it is smaller than the number:
+          if (ascendingArray[splitIndex] < toFind) {
+            // Make it the start of the active subarray.
+            startIndex = splitIndex;
+          }
+          // Otherwise, if it is greater than the number
+          else if (ascendingArray[splitIndex] > toFind) {
+            // Make it the end of the active subarray.
+            endIndex = splitIndex;
+          }
+          // Otherwise, i.e. if it is equal to the number:
+          else {
+            // Return its index.
+            return splitIndex;
+          }
+        }
+        /*
+          Otherwise, i.e. if the last element of the active subarray is smaller than the
+          number or is not comparable with the number:
+        */
+        else {
+          // Return a failure result.
+          return undefined;
         }
       }
       /*
-        Otherwise, i.e. if the last element of the active subarray is smaller than the
+        Otherwise, i.e. if the first element of the active subarray is greater than the
         number or is not comparable with the number:
       */
       else {
@@ -94,17 +106,9 @@ var binarySearch = function(toFind, ascendingArray) {
         return undefined;
       }
     }
-    /*
-      Otherwise, i.e. if the first element of the active subarray is greater than the
-      number or is not comparable with the number:
-    */
-    else {
-      // Return a failure result.
-      return undefined;
-    }
+    // The array does not contain an instance of the number. Return a failure result.
+    return undefined;
   }
-  // The array does not contain an instance of the number. Return a failure result.
-  return undefined;
 };
 
 export default binarySearch;
