e67d9f019aced37edc54ca3968deac813d32661a
Kamila Lambert <kamila.p.lambert@gmail.com>
2017-05-08 12:46:52 -0700

    makeChange passing all tests

--- src/makeChange.js
+++ src/makeChange.js
@@ -1,3 +1,26 @@
-export default function makeChange({price, amountGiven}) {
-  // your code here
+  export default function makeChange({price, amountGiven}) {
+
+  let changeNeeded = amountGiven - price,
+      coinChange = {quarters: 0, dimes: 0, nickels: 0, pennies: 0}
+
+  while (changeNeeded >= 25) {
+    coinChange.quarters++
+    changeNeeded -= 25
+  }
+
+  while (changeNeeded >= 10) {
+    coinChange.dimes++
+    changeNeeded -= 10
+  }
+
+  while (changeNeeded >= 5) {
+    coinChange.nickels++
+    changeNeeded -= 5
+  }
+
+  while (changeNeeded >= 1) {
+    coinChange.pennies++
+    changeNeeded -= 1
+  }
+  return coinChange
 }
